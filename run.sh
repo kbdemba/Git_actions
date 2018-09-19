ef2c9b9bb3cc6b4d5749676a79383eb3ccb5bf4b
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2018-09-19 11:15:01 -0700

    Point at images on docker hub

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@
 docker network create -d bridge mybridge
 
 docker run -d -v /db/index256:/db -e "ANN_INDEX_LENGTH=256" \
-    --network=mybridge --name nn_search nn_search
+    --network=mybridge --name nn_search cachengo/nn_search:1.0
 
-docker run -d --network=mybridge --name=facerec cachengo/facerec:latest
+docker run -d --network=mybridge --name=facerec cachengo/facerec:1.0
 
 docker run -it -p 5000:5000 -v /db/sso:/db -e "NN_SEARCH_ADDRESS=nn_search:5000" \
-  -e "FACEREC_ADDRESS=facerec:5000" --network=mybridge --name sso sso_example
+  -e "FACEREC_ADDRESS=facerec:5000" --network=mybridge --name sso cachengo/sso_example:1.0
