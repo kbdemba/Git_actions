fec82d719ecee24195e75ee4ad5d61f60a857380
kbdemba <kbdemba@gmail.com>
2020-05-05 18:57:03 -0500

    new branchcommit 3

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from app import db, login
 
 
-class User(UserMixin, db.Model): 
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=False)
     firstname = db.Column(db.String(64), index=False)
