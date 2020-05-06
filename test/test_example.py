b95de2a27278b09310626e75871dde9fa329cbe9
kbdemba <kbdemba@gmail.com>
2020-05-06 07:32:47 -0500

    adding unit test

---
+++ test/test_example.py
@@ -0,0 +1,10 @@
+import unittest
+from app import app
+
+
+class TestExample(unittest.TestCase):
+
+    def test_starter(self):
+        tester = app.test_client()
+        response = tester.get('/healthcheck')
+        self.assertEqual(response.status_code, 200)
