424252db9e1c2e0fce002e1df370f024f80022b7
kbdemba <kbdemba@gmail.com>
2020-05-06 07:45:17 -0500

    fixed test but faile lint test

--- test/test_example.py
+++ test/test_example.py
@@ -4,7 +4,7 @@ from app import app
 
 class TestExample(unittest.TestCase):
 
-    def test_starter(self):
+    def test_starter(self): 
         tester = app.test_client()
         response = tester.get('/healthcheck')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 200)
